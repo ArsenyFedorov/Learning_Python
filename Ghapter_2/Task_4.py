d43c8b5ad05a7ffbc6d0e27d7f83eff763cf0b59
Арсений <ars071026@example.com>
2023-05-29 17:49:55 +0500

    Task_4

---
+++ Ghapter_2/Task_4.py
@@ -0,0 +1,5 @@
+# сохранить имя пользователя и вывести его в нижнем и верхнем регистре и с капитализацией начальных букв
+user_name = input("Whats you name:")
+print(user_name.lower())
+print(user_name.upper())
+print(user_name.title())
